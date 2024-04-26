38bbf9f668b0c5ea9cdf869ee031e11a6bfacf36
Mithilesh Zavar <mithileshz@hotmail.co.uk>
2024-04-26 15:36:53 +0100

    Move Typescript test to intermediate

--- MK94.CodeGenerator.Test/Intermediate/TypescriptTests.cs
+++ MK94.CodeGenerator.Test/Intermediate/TypescriptTests.cs
@@ -17,6 +17,44 @@ namespace MK94.CodeGenerator.Test.Intermediate;
 public class TypescriptTests
 {
     [Test]
+    public void TestTs()
+    {
+        var c = new TypescriptCodeGenerator(new(new()));
+
+        var t = c
+            .File("filea.ts")
+            .Type("TypeA", MemberFlags.Public);
+
+        t.Property(MemberFlags.Public, TsTypeReference.ToType<int>(), "PropA");
+        t.Method(MemberFlags.Public, TsTypeReference.ToType<int>(), "MethodA")
+            .WithArgument(TsTypeReference.ToType<int>(), "a")
+            .WithArgument(TsTypeReference.ToType<int>(), "b")
+            .Body
+            .Append("return a + b;");
+
+
+        var t2 = c
+            .File("fileb.ts")
+            .Type("TypeB", MemberFlags.Public);
+
+        t2.Property(MemberFlags.Public, TsTypeReference.ToType<int>(), "PropA");
+        t2.Method(MemberFlags.Public, TsTypeReference.ToType<int>(), "MethodA")
+            .WithArgument(TsTypeReference.ToRaw("TypeA"), "c")
+            .WithArgument(TsTypeReference.ToRaw("TypeA"), "d")
+            .Body
+            .Append("return c + d;");
+
+        c.Generate(CodeBuilder.FactoryFromMemoryStream(out var files, IndentStyle.SameLine));
+
+        CodeBuilder.FlushAll();
+
+        foreach (var file in files)
+        {
+            DiskAssert.MatchesRaw(file.Key, Encoding.UTF8.GetString(file.Value.ToArray()).Replace("\r\n", "\n"));
+        }
+    }
+
+    [Test]
     public void Test()
     {
         var c = new TypescriptCodeGenerator(new(new()));
