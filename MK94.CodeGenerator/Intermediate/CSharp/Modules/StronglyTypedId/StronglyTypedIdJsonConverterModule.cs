92221c7e2c006ca83c7ee550fa9d79f5ee8b6627
mukut1994 <mukut1994@gmail.com>
2024-05-08 19:20:13 +0100

    Fix strongly typed json serializer for strings

--- MK94.CodeGenerator/Intermediate/CSharp/Modules/StronglyTypedId/StronglyTypedIdJsonConverterModule.cs
+++ MK94.CodeGenerator/Intermediate/CSharp/Modules/StronglyTypedId/StronglyTypedIdJsonConverterModule.cs
@@ -1,5 +1,6 @@
 ﻿using MK94.CodeGenerator.Features;
 using MK94.CodeGenerator.Intermediate.CSharp.Generator;
+using System;
 using System.Reflection;
 using System.Text.Json.Serialization;
 
@@ -22,7 +23,8 @@ public class StronglyTypedIdJsonConverterModule : IGeneratorModule<CSharpCodeGen
 
             foreach (var typeDef in fileDef.Types)
             {
-                if (typeDef.Type.GetCustomAttribute<StronglyTypedIdAttribute>() == null) continue;
+                var attribute = typeDef.Type.GetCustomAttribute<StronglyTypedIdAttribute>();
+                if (attribute == null) continue;
 
                 file.WithUsing("System.Text.Json");
                 file.WithUsing("System.Text.Json.Serialization");
@@ -37,12 +39,17 @@ public class StronglyTypedIdJsonConverterModule : IGeneratorModule<CSharpCodeGen
                     .Type($"{originalType.Name}Converter", MemberFlags.Public, CsharpTypeReference.ToRaw($"{originalType.Name}Converter"))
                     .WithInheritsFrom(CsharpTypeReference.ToRaw($"JsonConverter<{originalType.Name}>"));
 
-                converterClass
+                var method = converterClass
                     .Method(MemberFlags.Public | MemberFlags.Override, CsharpTypeReference.ToRaw(originalType.Name), "Read")
-                    .WithArgument(CsharpTypeReference.ToRaw("Utf8JsonReader"), "reader")
+                    // TODO "ref Utf8JsonReader" is a hack
+                    .WithArgument(CsharpTypeReference.ToRaw("ref Utf8JsonReader"), "reader")
                     .WithArgument(CsharpTypeReference.ToRaw("Type"), "typeToConvert")
-                    .WithArgument(CsharpTypeReference.ToRaw("JsonSerializerOptions"), "options")
-                    .Body.Append($"return new {originalType.Name}(Guid.Parse(reader.GetString()!));");
+                    .WithArgument(CsharpTypeReference.ToRaw("JsonSerializerOptions"), "options");
+
+                if(attribute.Type == typeof(Guid))
+                    method.Body.Append($"return new {originalType.Name}(Guid.Parse(reader.GetString()!));");
+                else
+                    method.Body.Append($"return new {originalType.Name}(reader.GetString()!);");
 
                 converterClass
                     .Method(MemberFlags.Public | MemberFlags.Override, CsharpTypeReference.ToVoid(), "Write")
